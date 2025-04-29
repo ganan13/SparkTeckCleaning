d5ebb789d0410a1ab7ea6d55e05e4b429cf2e459
Ezio <sajeethan@smartzi.com>
2025-04-29 22:45:27 +0530

    image path updated

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Carousel.css'
+
+function Carousel() {
+  return (
+    <div id="carouselExampleFade" className="carousel slide carousel-fade">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://ganan13.github.io/SparkTeckCleaning/src/assets/carouselImage/image1.png" className="carousel-img" alt="slide 1" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://ganan13.github.io/SparkTeckCleaning/src/assets/carouselImage/image2.png" className="carousel-img" alt="slide 2" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://ganan13.github.io/SparkTeckCleaning/src/assets/carouselImage/image3.png" className="carousel-img" alt="slide 3" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  )
+}
+
+export default Carousel
