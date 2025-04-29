eeaa6b98cc6dabbc27ebc63b7a5a38d9a569b19c
ganan siva <neelaganan@smartzi.com>
2025-04-29 22:07:53 +0530

    changes added

--- src/Configurations/form-configs.js
+++ src/Configurations/form-configs.js
@@ -1,72 +1,60 @@
 export const cleaningServiceType = [
     {
-        name: "Service type A",
+        name: "Interior Builders Cleaning",
         value: 1
     },
     {
-        name: "Service type B",
+        name: "Exterior Builders Cleaning",
         value: 2
     },
     {
-        name: "Service type c",
+        name: "Builders Windows Cleaning",
         value: 3
     },
     {
-        name: "Service type D",
+        name: "Total Builders Cleaning Solution",
         value: 4
-    },
-    {
-        name: "Service type E",
-        value: 5
-    },
+    },    
 ]
 
 export const useOfService = [
     {
-        name: "Use type A",
+        name: "Commercial Purpose",
         value: 1
     },
     {
-        name: "Use type B",
+        name: "Residential Cleaning",
         value: 2
     },
     {
-        name: "Use type c",
+        name: "Industrial Cleaning",
         value: 3
-    },
-    {
-        name: "Use type D",
-        value: 4
-    },
-    {
-        name: "Use type E",
-        value: 5
-    },
+    },    
 ]
 
-export const purposeOfInquiry = [
-    {
-        name: "Inquiry type A",
-        value: 1
-    },
-    {
-        name: "Inquiry type B",
-        value: 2
-    },
-    {
-        name: "Inquiry type c",
-        value: 3
-    },
-    {
-        name: "Inquiry type D",
-        value: 4
-    },
-    {
-        name: "Inquiry type E",
-        value: 5
-    },
-]
+// export const purposeOfInquiry = [
+//     {
+//         name: "Inquiry type A",
+//         value: 1
+//     },
+//     {
+//         name: "Inquiry type B",
+//         value: 2
+//     },
+//     {
+//         name: "Inquiry type c",
+//         value: 3
+//     },
+//     {
+//         name: "Inquiry type D",
+//         value: 4
+//     },
+//     {
+//         name: "Inquiry type E",
+//         value: 5
+//     },
+// ]
 
 export const scriptConfig = {
-    deploymentId: 'AKfycbw2b-T-5MvbB1t4MjtAQ1sM6pXC-217tU_qC9sPyy7c3va3-Hf9zGwla6QLA2z8Ptnw'
+    deploymentId: 'AKfycbxzk8pjRfXskml9FfQQvXcdFJSbW_4bbzKXZhzkPZv2e0ydEksi9JRCFNV1Hl7OGBIG'
 }
